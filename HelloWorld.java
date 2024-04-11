da61d5a9b63ec3a17d5651d276707d5a684e1f78
mrsrivas01 <164207780+mrsrivas01@users.noreply.github.com>
2024-04-11 11:47:16 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
+import java.io.*;
+import java.util.*;
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(new OutputStreamWriter(System.out));
+        out.println("Hello World");
+        out.close();
     }
 }
